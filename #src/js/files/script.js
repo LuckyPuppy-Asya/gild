555e3790ce7874f733cfe4433acccbf2e4cee08a
Asya <asya.halickova@yandex.ru>
2021-08-21 22:29:49 +0300

    exhibition

--- #src/js/files/script.js
+++ #src/js/files/script.js
@@ -11,7 +11,7 @@ window.onload = function () {
 	function documentActions(e) {
 		const targetElement = e.target;
 		// console.log(targetElement, 111);
-
+		// console.log(targetElement.closest('.custom-select__top'), 222);
 		//по клику на желтые пункты на фото срабатывают табы========================================================================================================================================================
 
 		if (targetElement.classList.contains('details__dot') && targetElement.getAttribute('data-dot')) {
@@ -32,7 +32,7 @@ window.onload = function () {
 				});
 			}
 		}
-		//работа фильтров Сортировать и Вид в блоке catalog========================================================================================================================================================
+		//работа фильтров "Сортировать" в блоке catalog========================================================================================================================================================
 		if (targetElement.classList.contains('custom-select__top')) {
 			targetElement.closest('.custom-select').classList.toggle('custom-select--open');
 		}
@@ -46,6 +46,39 @@ window.onload = function () {
 			targetElement.textContent = topText;
 			targetElement.closest('.custom-select').classList.toggle('custom-select--open');
 		}
+
+		//работа кнопок "Вид" в блоке catalog========================================================================================================================================================
+		if (targetElement.classList.contains('custom-select__btn') && targetElement.closest('.custom-select__top')) {
+			targetElement.closest('.custom-select').classList.toggle('custom-select--open');
+		}
+		if (targetElement.classList.contains('custom-select__btn') && !targetElement.closest('.custom-select__top')) {
+
+			let blockTop = targetElement.closest('.custom-select').querySelector('.custom-select__top');
+			let blockItem = targetElement.closest('.custom-select__item');
+			let blockTopOld = blockTop.querySelector('.custom-select__btn');
+			let columns = targetElement.dataset.columns;
+			let exhibitionRow = document.querySelector('.exhibition__row');
+
+			blockTopOld.classList.remove('_current');
+			blockTop.innerHTML = '';
+			blockTop.appendChild(targetElement);
+			targetElement.classList.add('_current');
+			blockItem.appendChild(blockTopOld);
+			targetElement.closest('.custom-select').classList.toggle('custom-select--open');
+			exhibitionRow.dataset.gridColumns = columns;
+
+
+
+			// let text = targetElement.textContent;
+			// let topBlock = targetElement.closest('.custom-select').querySelector('.custom-select__top');
+			// let topText = topBlock.textContent;
+
+			// topBlock.textContent = text;
+			// targetElement.textContent = topText;
+			// targetElement.closest('.custom-select').classList.toggle('custom-select--open');
+		}
+
+
 
 
 	}
